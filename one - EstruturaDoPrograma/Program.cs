0faa9eb7952bb122828276c25c853fc7fcfa630c
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-01-31 16:41:02 -0300

    implementing a stack

---
+++ one - EstruturaDoPrograma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace one___EstruturaDoPrograma
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            // Console.WriteLine(s.Desempilha()); para forçar uma exception
+
+        }
+    }
+}
